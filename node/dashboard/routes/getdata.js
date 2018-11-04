68e624349d78ad7a2d4c1b4b7d9ce5d8bf47cfed
victor <victor@company.com>
2018-11-04 17:28:06 +0000

    Removed commnted-out/redundant code

--- node/dashboard/routes/getdata.js
+++ node/dashboard/routes/getdata.js
@@ -15,20 +15,6 @@ router.get(Object.keys(config.table2fields).map(x => '/querydb/' + x), function(
 	var fields = Object.keys(config.table2fields[table])
 	var sqlFields = Object.values(config.table2fields[table])
 	req.sqlcon.query('SELECT * FROM ' + table, function(err, result) {
-		// console.log(result[0][config.table2fields[table][fields[0]]]);
-		// console.log(result[0][config.table2fields[table][fields[1]]]);
-		// res.render('querydb', {fields: fields, data: result});
-		// console.log(sqlFields);
-		// res.render('querydb', {fields: fields, sqlFields: sqlFields, data: result});
-
-		// console.log(result.length);
-
-		// for (var i = 0; i < result.length; i++) {
-		// 	var row = result[i];
-		// 	for (var j = 0; j < sqlFields.length; j++) {
-		// 		console.log(result[i][sqlFields[j]]);
-		// 	}
-		// }
 		res.render('querydb', {fields: fields, sqlFields: sqlFields, data: result});
 	});
 	// res.render('querydb', {fields: fields});
